38b3aff21a84278da78487ebed56b0733d292133
Kevin Liu <kliu0862@bths.edu>
2020-02-28 10:44:18 -0500

    Templates for Paper and User

---
+++ Paper.java
@@ -0,0 +1,178 @@
+/*
+ * @course ISTE.330.01
+ * @version Project.01
+ * @author Andrea Pallotta, Kevin Liu, Blake Wesel
+ */
+ 
+public class Paper
+{
+   //Paper object info
+   private int paperId;
+   private String filename;
+   private String[] subjects;
+   
+   //co-authors info
+   private String[] firstnames;
+   private String[] lastnames;
+   
+   //submission info
+   private String submissionTitle;
+   private String submissionAbstract;
+   private int submissionType;
+
+   /*
+      @param none
+      default constructor for Paper object
+      initializes blank or unharmful values
+      @return none
+   */
+   public Paper()
+   {
+      paperId = 0;
+      filename = "";
+      submissionTitle = "";
+      submissionAbstract = "";
+      submissionType = 0;
+   }
+   
+   /*
+      @param (int)
+      returns all info for a specific paper EXCEPT filename
+      @return String
+   */
+   public String getPaper(int _paperId)
+   {
+      //create temp String to store full names
+      String tempNames = "";
+      //iterate through length of array for names
+      for (int i = 0; i < firstnames.length; i++)
+      {
+         //concatenate first name and last name together
+         if (i == 0)
+         {
+            tempNames += (firstnames[i] + " " + lastnames[i]);
+         }
+         else
+         {
+            tempNames += (", " + firstnames[i] + " " + lastnames[i]);
+         }
+      }
+      
+      //create temp String to store subjects
+      String tempSubs = "";
+      for (int i = 0; i < subjects.length; i++)
+      {
+         //contatenate subjects into one String for display
+         if (i == 0)
+         {
+            tempSubs += subjects[i];
+         }
+         else
+         {
+            tempNames += (", " + subjects[i]);
+         }
+      }
+   
+      return ("Paper ID:" + paperId +
+               "\nSubmission Title:" + submissionTitle +
+               "\nSubmission Abstract:" + submissionAbstract +
+               "\nSubmission Type:" + submissionType +
+               "\nSubjects: " + tempSubs +
+               "\nCo-Authors:" + tempNames);
+
+
+
+   }
+   
+   /*
+      @param (int,String,String,int,String,String[],String[],String[])
+      default constructor for Paper object
+      @return none
+   */
+   public void setPaper(int _paperId, String _submissionTitle, String _submissionAbstract, int _submissionType, String _filename, String[] _subjects, String[] _firstNames, String[] _lastnames)
+   {
+   
+   }
+   
+   //accessors
+   public int getId()
+   {
+      return paperId;
+   }
+   
+   public String getSubTitle()
+   {
+      return submissionTitle;
+   }
+   
+   public String getSubAbstract()
+   {
+      return submissionAbstract;
+   }
+   
+   public int getSubType()
+   {
+      return submissionType;
+   }
+   
+   public String getFileName()
+   {
+      return filename;
+   }
+   
+   public String[] getSubjects()
+   {
+      return subjects;
+   }
+   
+   public String[] getFirstNames()
+   {
+      return firstnames;
+   }
+   
+   public String[] getLastNames()
+   {
+      return lastnames;
+   }
+   
+   //mutators
+   public void setId(int _paperId)
+   {
+      paperId = _paperId;
+   }
+   
+   public void setSubTitle(String _submissionTitle)
+   {
+      submissionTitle = _submissionTitle;
+   }
+   
+   public void setSubAbstract(String _submissionAbstract)
+   {
+      submissionAbstract = _submissionAbstract;
+   }
+   
+   public void setSubType(int _submissionType)
+   {
+      submissionType = _submissionType;
+   }
+   
+   public void setFileName(String _filename)
+   {
+      filename = _filename;
+   }
+   
+   public void setSubjects(String[] _subjects)
+   {
+      subjects = _subjects;
+   }
+   
+   public void setFirstNames(String[] _firstnames)
+   {
+      firstnames = _firstnames;
+   }
+   
+   public void setLastNames(String[] _lastnames)
+   {
+      lastnames = _lastnames;
+   }
+}
---
+++ User.java
@@ -0,0 +1,144 @@
+/*
+ * @course ISTE.330.01
+ * @version Project.01
+ * @author Andrea Pallotta, Kevin Liu, Blake Wesel
+ */
+ 
+public class User
+{
+   //User object info
+   private String lastName;
+   private String firstName;
+   private String email;
+   private String pswd;
+   private String affiliation;
+   
+   /*
+      @param none
+      default constructor for User object
+      initializes blank or unharmful values
+      @return none
+   */
+   public User()
+   {
+      lastName = "";
+      firstName = "";
+      String email = "";
+      String pswd = "";
+      String affiliation = "";
+   }
+   
+   /*
+      @param (int)
+      returns all papers for specified user
+      @return Paper[]
+   */
+   public Paper[] getPapers(int _userId)
+   {
+      return null;
+   }
+   
+   /*
+      @param none
+      returns attributes of User EXCEPT password
+      @return String
+   */
+   public String getUser()
+   {
+      return "";
+   }
+   
+   /*
+      @param (String,String,String,String,String)
+      updates User attributes to db
+      creates new entry if no matching userId
+      @return none
+   */
+   public void setUser(String _lastName, String _firstName, String _email, String _pswd, String _affiliation)
+   {
+      
+   }
+   
+   /*
+      @param (String)
+      creates new password and emails to specified address
+      ONLY GOOD FOR 5 MINUTES      
+      @return none
+   */
+   public void resetPassword(String _email)
+   {
+   
+   }
+   
+   /*
+      @param (String)
+      changes password of currently logged in user to param
+      @return none
+   */
+   public void setPassword(String _password)
+   {
+   
+   }
+   
+   /*
+      @param (String,String)
+      IF good credentials, returns token
+      @return token?
+   */
+   public void login(String _email, String _password)
+   {
+   
+   }
+   
+   //accessors
+   public String getLastName()
+   {
+      return lastName;
+   }
+   
+   public String getFirstName()
+   {
+      return firstName;
+   }
+   
+   public String getEmail()
+   {
+      return email;
+   }
+   
+   public String getPswd()
+   {
+      return pswd;
+   }
+   
+   public String getAffil()
+   {
+      return affiliation;
+   }
+   
+   //mutators
+   public void setLastName(String _lastName)
+   {
+      lastName = _lastName;
+   }
+   
+   public void setFirstName(String _firstName)
+   {
+      lastName = _firstName;
+   }
+   
+   public void setEmail(String _email)
+   {
+      email = _email;
+   }
+   
+   public void setPswd(String _pswd)
+   {
+      pswd = _pswd;
+   }
+   
+   public void setAffil(String _affiliation)
+   {
+      affiliation = _affiliation;
+   }
+}
